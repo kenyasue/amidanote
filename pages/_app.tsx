e6f0f79d25890820279062c22467af1f9512ee66
ken yasue <ken@yasue.us>
2021-03-14 20:04:42 +0100

    better mobile styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/vars.scss";
 import "../styles/global.scss";
 import type { AppProps /*, AppContext */ } from "next/app";
 import { Provider } from "next-auth/client";
+import Head from "next/head";
 
 import { useReducer } from "react";
 import { appStateContext, dispatcherContext } from "../lib/reducer/context";
@@ -17,6 +18,12 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <Provider session={pageProps.session}>
       <appStateContext.Provider value={state}>
         <dispatcherContext.Provider value={dispatch}>
+          <Head>
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
+          </Head>
           <Component {...pageProps} />
         </dispatcherContext.Provider>
       </appStateContext.Provider>

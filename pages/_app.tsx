ca36714c1f510e6c2ec92e2b73b0e27ef5875d08
ken yasue <ken@yasue.us>
2021-04-22 10:52:27 +0200

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,6 +30,27 @@ export default function MyApp({ Component, pageProps }: AppProps) {
               href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
               rel="stylesheet"
             ></link>
+
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/favicon/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/favicon/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/favicon/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/favicon/site.webmanifest" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="theme-color" content="#ffffff"></meta>
           </Head>
           <Component {...pageProps} />
         </dispatcherContext.Provider>

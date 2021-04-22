5427bb74470be36ec7913fa507b04d67e40f3ab2
ken yasue <ken@yasue.us>
2021-04-22 04:56:48 +0200

    footer content

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,1 +1,13 @@
-export default () => <>Footer</>;
+import { GithubOutlined } from "@ant-design/icons";
+
+export default () => (
+  <>
+    Open{" "}
+    <a href="https://github.com/kenyasue/lakumemo">
+      <GithubOutlined />
+      &nbsp; github
+    </a>{" "}
+    repository | Developed by{" "}
+    <a href="https://clover.studio">Clover Studio Ltd.</a>
+  </>
+);

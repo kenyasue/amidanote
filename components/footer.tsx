7fd2e112bc56298f6aac08529df389df7de0038d
ken yasue <ken@yasue.us>
2021-04-22 05:06:55 +0200

    add target _blank to footer links

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,13 @@ import { GithubOutlined } from "@ant-design/icons";
 export default () => (
   <>
     Open{" "}
-    <a href="https://github.com/kenyasue/lakumemo">
+    <a href="https://github.com/kenyasue/lakumemo" target="_blank">
       <GithubOutlined />
       &nbsp; github
     </a>{" "}
     repository | Developed by{" "}
-    <a href="https://clover.studio">Clover Studio Ltd.</a>
+    <a href="https://clover.studio" target="_blank">
+      Clover Studio Ltd.
+    </a>
   </>
 );

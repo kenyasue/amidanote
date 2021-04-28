ec3cb3b42c09a5b83b8da7bd9462587edacaf8b6
ken yasue <ken@yasue.us>
2021-04-28 21:36:05 +0200

    standalone mode only as default

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -24,50 +24,9 @@ if (process.env.NODE_ENV === "production") {
   }
   prisma = global.prisma;
 }
+
 const options = {
-  providers: [
-    Providers.Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorizationUrl:
-        "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
-    }),
-    {
-      id: "standalone",
-      name: "StandaloneMode",
-      type: "oauth",
-      version: "2.0",
-      params: { grant_type: "authorization_code" },
-
-      /*
-      accessTokenUrl: `http://localhost:3030/oauth/token`,
-      requestTokenUrl: `http://localhost:3030/oauth/token`,
-      authorizationUrl: `http://localhost:3030/oauth?response_type=code`,
-      profileUrl: `http://localhost:3030/secure`,
-      */
-
-      accessTokenUrl: `${process.env.NEXTAUTH_URL}/api/auth/provider/token`,
-      requestTokenUrl: `${process.env.NEXTAUTH_URL}/api/auth/provider/token`,
-      authorizationUrl: `${process.env.NEXTAUTH_URL}/api/auth/provider/authenticate?response_type=code`,
-      profileUrl: `${process.env.NEXTAUTH_URL}/api/auth/provider/profile`,
-
-      async profile(profile: any, tokens: any) {
-        console.log("provider profile", profile);
-        console.log("provider tokens", tokens);
-        // You can use the tokens, in case you want to fetch more profile information
-        // For example several OAuth provider does not return e-mail by default.
-        // Depending on your provider, will have tokens like `access_token`, `id_token` and or `refresh_token`
-        return {
-          id: profile.id,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-        };
-      },
-      clientId: "myClientId",
-      clientSecret: "myClientSecret",
-    },
-  ],
+  providers: [] as Array<any>,
   pages: {
     signIn: "/home",
     signOut: "/",
@@ -75,5 +34,45 @@ const options = {
   },
   adapter: Adapters.Prisma.Adapter({ prisma }),
 };
+
+if (process.env.STANDALONEMODE) {
+  options.providers.push({
+    id: "standalone",
+    name: "StandaloneMode",
+    type: "oauth",
+    version: "2.0",
+    params: { grant_type: "authorization_code" },
+
+    accessTokenUrl: `${process.env.NEXTAUTH_URL}/api/auth/provider/token`,
+    requestTokenUrl: `${process.env.NEXTAUTH_URL}/api/auth/provider/token`,
+    authorizationUrl: `${process.env.NEXTAUTH_URL}/api/auth/provider/authenticate?response_type=code`,
+    profileUrl: `${process.env.NEXTAUTH_URL}/api/auth/provider/profile`,
+
+    async profile(profile: any, tokens: any) {
+      console.log("provider profile", profile);
+      console.log("provider tokens", tokens);
+      // You can use the tokens, in case you want to fetch more profile information
+      // For example several OAuth provider does not return e-mail by default.
+      // Depending on your provider, will have tokens like `access_token`, `id_token` and or `refresh_token`
+      return {
+        id: profile.id,
+        name: profile.name,
+        email: profile.email,
+        image: profile.picture,
+      };
+    },
+    clientId: "myClientId",
+    clientSecret: "myClientSecret",
+  });
+} else {
+  options.providers.push(
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorizationUrl:
+        "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
+    })
+  );
+}
 export default (req: NextApiRequest, res: NextApiResponse) =>
   NextAuth(req, res, options);

a9a3494a22314c59c2a169779a090b07f5238516
ken yasue <ken@yasue.us>
2021-09-14 18:12:51 +0200

    fix for webpack

--- next.config.js
+++ next.config.js
@@ -19,12 +19,7 @@ module.exports = {
       }
   },
   webpack: (config, { isServer,webpack }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
+
 
     config.plugins.push(new webpack.DefinePlugin({ "global.GENTLY": false }));
 

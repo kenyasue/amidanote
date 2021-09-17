b023f3e409b23e3a668a2484c31c420fb393ec2e
ken yasue <ken@yasue.us>
2021-09-17 09:14:14 +0200

    fix swagger again

--- pages/swagger.tsx
+++ pages/swagger.tsx
@@ -1,5 +1,5 @@
 import SwaggerUI from "swagger-ui-react";
 
 export default function Home() {
-  return <SwaggerUI url="http://localhost:3000/api/swagger" />;
+  return <SwaggerUI url="https://app.amidanote.com/api/swagger" />;
 }

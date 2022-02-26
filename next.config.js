bf0861079e895ef9062a21f0d996368ba0d15c3b
ken yasue <ken@clover.studio>
2022-02-27 00:58:36 +0100

    save changes

--- next.config.js
+++ next.config.js
@@ -21,9 +21,7 @@ module.exports = {
   webpack: (config, { isServer,webpack }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
+      config.resolve.fallback.fs = false;
     }
 
     config.plugins.push(new webpack.DefinePlugin({ "global.GENTLY": false }));
